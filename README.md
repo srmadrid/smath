79a02f94e5da601cc723f0c501b42d982e850def
sergio8735 <127796005+sergio8735@users.noreply.github.com>
2023-03-19 15:30:00 +0100

    Removed the vector_ and matrix_ prefixes and replaced them with vector:: and matrix:: namespaces. Also added an algebra:: namespace.

--- README.md
+++ README.md
@@ -2,23 +2,24 @@
 This small repository holds a .hpp file containing some useful algebra operations using vectors and matrices. 
 It is not meant to be taken seriously, as this is a test repository to begin leaning the platform and the workflow.
 
-### Functions list:
+## Functions list:
+### Algebra
 | Function              | Description                                                            | Input          | Output |
 |-----------------------|------------------------------------------------------------------------|----------------|--------|
-| vector_print          | Prints the input vector                                                | vector         | void   |
-| vector_addition       | Returns the result of adding the input vectors                         | vector, vector | vector |
-| vector_subtraction    | Returns the result of subtracting the input vectors                    | vector, vector | vector |
-| vector_modulus        | Returns the modulus of the input vector                                | vector         | double |
-| vector_scalar         | Returns the result of multiplying the input vector by the input scalar | vector, double | vector |
-| vector_dot            | Returns the [dot product](https://en.wikipedia.org/wiki/Dot_product) of the input vectors | vector, vector | double |
-| vector_cross          | Returns the [cross product](https://en.wikipedia.org/wiki/Cross_product) of the input vectors | vector, vector | vector |
-| vector_projection     | In development                                                         |                |        |
-| matrix_print          | Prints the input matrix                                                | matrix         | void   |
-| matrix_identity       | Returns the identity matrix of the specified dimensions (n x m)        | int, int       | matrix |
-| matrix_transpose      | Returns the [transpose](https://en.wikipedia.org/wiki/Transpose) of the input matrix | matrix | matrix |
-| matrix_addition       | Returns the result of adding the input matrices                        | matrix, matrix | matrix |
-| matrix_subtraction    | Returns the result of subtracting the input matrices                   | matrix, matrix | matrix |
-| matrix_scalar         | Returns the result of multiplying the input matrix by the input scalar | matrix, double | matrix |
-| matrix_multiplication | Returns the result of [multiplying](https://en.wikipedia.org/wiki/Matrix_multiplication) the input matrices | matrix, matrix | matrix |
-| matrix_power          | Returns the result of calculating the xth power of the input matrix; if x is 0, returns the identity matrix with the size of the input matrix | matrix, int    | matrix |
+| vector::print          | Prints the input vector                                                | vector         | void   |
+| vector::addition       | Returns the result of adding the input vectors                         | vector, vector | vector |
+| vector::subtraction    | Returns the result of subtracting the input vectors                    | vector, vector | vector |
+| vector::modulus        | Returns the modulus of the input vector                                | vector         | double |
+| vector::scalar         | Returns the result of multiplying the input vector by the input scalar | vector, double | vector |
+| vector::dot            | Returns the [dot product](https://en.wikipedia.org/wiki/Dot_product) of the input vectors | vector, vector | double |
+| vector::cross          | Returns the [cross product](https://en.wikipedia.org/wiki/Cross_product) of the input vectors | vector, vector | vector |
+| vector::projection     | In development                                                         |                |        |
+| matrix::print          | Prints the input matrix                                                | matrix         | void   |
+| matrix::identity       | Returns the identity matrix of the specified dimensions (n x m)        | int, int       | matrix |
+| matrix::transpose      | Returns the [transpose](https://en.wikipedia.org/wiki/Transpose) of the input matrix | matrix | matrix |
+| matrix::addition       | Returns the result of adding the input matrices                        | matrix, matrix | matrix |
+| matrix::subtraction    | Returns the result of subtracting the input matrices                   | matrix, matrix | matrix |
+| matrix::scalar         | Returns the result of multiplying the input matrix by the input scalar | matrix, double | matrix |
+| matrix::multiplication | Returns the result of [multiplying](https://en.wikipedia.org/wiki/Matrix_multiplication) the input matrices | matrix, matrix | matrix |
+| matrix::power          | Returns the result of calculating the xth power of the input matrix; if x is 0, returns the identity matrix with the size of the input matrix | matrix, int    | matrix |
 
--- src/algebra.hpp
+++ src/algebra.hpp
@@ -2,160 +2,162 @@
 #include <iostream>
 #include <cmath>
 
-
-void vector_print(std::vector<double> &V){
-    for(double i: V){
-        std::cout << i << " ";
-    }std::cout << std::endl;
-}
-
-
-std::vector<double> vector_addition(std::vector<double> V, std::vector<double> &W){
-    // Assumes input vectors have equal dimensions
-    for(int i {0}; i < V.size(); ++i){
-        V[i] += W[i];
-    }
-    return V;
-}
+namespace algebra{
+    namespace vector{
+        void print(std::vector<double> &V){
+            for(double i: V){
+                std::cout << i << " ";
+            }std::cout << std::endl;
+        }
 
 
-std::vector<double> vector_subtraction(std::vector<double> V, std::vector<double> &W){
-    // Assumes input vectors have equal dimensions
-    for(int i {0}; i < V.size(); ++i){
-        V[i] -= W[i];
-    }
-    return V;
-}
+        std::vector<double> addition(std::vector<double> V, std::vector<double> &W){
+            // Assumes input vectors have equal dimensions
+            for(int i {0}; i < V.size(); ++i){
+                V[i] += W[i];
+            }
+            return V;
+        }
 
 
-double vector_modulus(std::vector<double> &V){
-    double result {0};
-    for(double i: V){
-        result += std::pow(i, 2);
-    }
-    return std::sqrt(result);
-}
+        std::vector<double> subtraction(std::vector<double> V, std::vector<double> &W){
+            // Assumes input vectors have equal dimensions
+            for(int i {0}; i < V.size(); ++i){
+                V[i] -= W[i];
+            }
+            return V;
+        }
 
 
-std::vector<double> vector_scalar(std::vector<double> V, double x){
-    for(int i {0}; i < V.size(); ++i){
-        V[i] *= x;
-    }
-    return V;
-}
+        double modulus(std::vector<double> &V){
+            double result {0};
+            for(double i: V){
+                result += std::pow(i, 2);
+            }
+            return std::sqrt(result);
+        }
 
 
-double vector_dot(std::vector<double> &V, std::vector<double> &W){
-    // Assumes input vectors have equal lengths
-    double result {};
-    for(int i {0}; i < V.size(); ++i){
-        result += V[i]*W[i];
-    }
-    return result;
-}
+        std::vector<double> scalar(std::vector<double> V, double x){
+            for(int i {0}; i < V.size(); ++i){
+                V[i] *= x;
+            }
+            return V;
+        }
 
 
-std::vector<double> vector_cross(std::vector<double> &V, std::vector<double> &W){
-    // Assumes input vectors have 3 components
-    std::vector<double> X {0, 0, 0};
-    X[0] += V[1]*W[2] - V[2]*W[1];
-    X[1] += V[2]*W[0] - V[0]*W[2];
-    X[2] += V[0]*W[1] - V[1]*W[0];
-    return X;
-}
+        double dot(std::vector<double> &V, std::vector<double> &W){
+            // Assumes input vectors have equal lengths
+            double result {};
+            for(int i {0}; i < V.size(); ++i){
+                result += V[i]*W[i];
+            }
+            return result;
+        }
 
 
-std::vector<double> vector_projection(std::vector<double> V, std::vector<double> W){
-    // To be developed
-    return std::vector<double> {1, 2, 3};
-}
+        std::vector<double> cross(std::vector<double> &V, std::vector<double> &W){
+            // Assumes input vectors have 3 components
+            std::vector<double> X {0, 0, 0};
+            X[0] += V[1]*W[2] - V[2]*W[1];
+            X[1] += V[2]*W[0] - V[0]*W[2];
+            X[2] += V[0]*W[1] - V[1]*W[0];
+            return X;
+        }
 
 
-void matrix_print(std::vector<std::vector<double>> &A){
-    for(std::vector<double> &V: A){
-        vector_print(V);
-    }
-}
-
-
-std::vector<std::vector<double>> matrix_identity(int n, int m){
-    std::vector<std::vector<double>> A (n, std::vector<double> (m, 0));
-    for(int i {0}; i < A.size(); ++i){
-        for(int j {0}; j < A[0].size(); ++j){
-            if(i == j){
-                A[i][j] = 1;
+        std::vector<double> projection(std::vector<double> V, std::vector<double> W){
+            // To be developed
+            return std::vector<double> {1, 2, 3};
+        }
+    }namespace matrix{
+        void print(std::vector<std::vector<double>> &A){
+            for(std::vector<double> &V: A){
+                vector::print(V);
             }
         }
-    }
-    return A;
-}
 
 
-std::vector<std::vector<double>> matrix_transpose(std::vector<std::vector<double>> &A){
-    std::vector<std::vector<double>> B (A[0].size(), std::vector<double> (A.size(), 0));
-    for(int i {0}; i < A.size(); ++i){
-        for(int j {0}; j < A[0].size(); ++j){
-            B[j][i] = A[i][j];
+        std::vector<std::vector<double>> identity(int n, int m){
+            std::vector<std::vector<double>> A (n, std::vector<double> (m, 0));
+            for(int i {0}; i < A.size(); ++i){
+                for(int j {0}; j < A[0].size(); ++j){
+                    if(i == j){
+                        A[i][j] = 1;
+                    }
+                }
+            }
+            return A;
         }
-    }
-    return B;
-}
 
 
-std::vector<std::vector<double>> matrix_addition(std::vector<std::vector<double>> A, std::vector<std::vector<double>> &B){
-    // Assumes input matrices have equal dimensions
-    for(int i {0}; i < A.size(); ++i){
-        for(int j {0}; j < A[0].size(); ++j){
-            A[i][j] += B[i][j];
+        std::vector<std::vector<double>> transpose(std::vector<std::vector<double>> &A){
+            std::vector<std::vector<double>> B (A[0].size(), std::vector<double> (A.size(), 0));
+            for(int i {0}; i < A.size(); ++i){
+                for(int j {0}; j < A[0].size(); ++j){
+                    B[j][i] = A[i][j];
+                }
+            }
+            return B;
         }
-    }
-    return A;
-}
 
 
-std::vector<std::vector<double>> matrix_subtraction(std::vector<std::vector<double>> A, std::vector<std::vector<double>> &B){
-    // Assumes input matrices have equal dimensions
-    for(int i {0}; i < A.size(); ++i){
-        for(int j {0}; j < A[0].size(); ++j){
-            A[i][j] -= B[i][j];
+        std::vector<std::vector<double>> addition(std::vector<std::vector<double>> A, std::vector<std::vector<double>> &B){
+            // Assumes input matrices have equal dimensions
+            for(int i {0}; i < A.size(); ++i){
+                for(int j {0}; j < A[0].size(); ++j){
+                    A[i][j] += B[i][j];
+                }
+            }
+            return A;
         }
-    }
-    return A;
-}
 
 
-std::vector<std::vector<double>> matrix_scalar(std::vector<std::vector<double>> A, double x){
-    for(int i {0}; i < A.size(); ++i){
-        for(int j {0}; j < A[0].size(); ++j){
-            A[i][j] *= x;
+        std::vector<std::vector<double>> subtraction(std::vector<std::vector<double>> A, std::vector<std::vector<double>> &B){
+            // Assumes input matrices have equal dimensions
+            for(int i {0}; i < A.size(); ++i){
+                for(int j {0}; j < A[0].size(); ++j){
+                    A[i][j] -= B[i][j];
+                }
+            }
+            return A;
         }
-    }
-    return A;
-}
 
 
-std::vector<std::vector<double>> matrix_multiplication(std::vector<std::vector<double>> A, std::vector<std::vector<double>> &B){
-    // Assumes input matrices have correct dimensions
-    std::vector<std::vector<double>> C (A.size(), std::vector<double> (B[0].size(), 0));
-    for(int i {0}; i < A.size(); ++i){
-        for(int j {0}; j < B[0].size(); ++j){
-            for(int n {0}; n < A[0].size(); ++n){
-                C[i][j] += A[i][n]*B[n][j];
+        std::vector<std::vector<double>> scalar(std::vector<std::vector<double>> A, double x){
+            for(int i {0}; i < A.size(); ++i){
+                for(int j {0}; j < A[0].size(); ++j){
+                    A[i][j] *= x;
+                }
+            }
+            return A;
+        }
+
+
+        std::vector<std::vector<double>> multiplication(std::vector<std::vector<double>> A, std::vector<std::vector<double>> &B){
+            // Assumes input matrices have correct dimensions
+            std::vector<std::vector<double>> C (A.size(), std::vector<double> (B[0].size(), 0));
+            for(int i {0}; i < A.size(); ++i){
+                for(int j {0}; j < B[0].size(); ++j){
+                    for(int n {0}; n < A[0].size(); ++n){
+                        C[i][j] += A[i][n]*B[n][j];
+                    }
+                }
+            }
+            return C;
+        }
+
+
+        std::vector<std::vector<double>> power(std::vector<std::vector<double>> A, int x){
+            if(x == 0){
+                return identity(A.size(), A.size());
+            }else{
+                std::vector<std::vector<double>> B {A};
+                for(int i {0}; i < x - 1; ++i){
+                    B = multiplication(B, A);
+                }
+                return B;
             }
         }
-    }
-    return C;
-}
-
-
-std::vector<std::vector<double>> matrix_power(std::vector<std::vector<double>> A, int x){
-    if(x == 0){
-        return matrix_identity(A.size(), A.size());
-    }else{
-        std::vector<std::vector<double>> B {A};
-        for(int i {0}; i < x - 1; ++i){
-            B = matrix_multiplication(B, A);
-        }
-        return B;
     }
 }
